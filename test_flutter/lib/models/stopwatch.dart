2c3c37bca622082bda62df05a499d87fb6a26ab4
ume_bar <kagari0915@gmail.com>
2022-09-16 10:21:29 +0900

    add environment

---
+++ test_flutter/lib/models/stopwatch.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+import 'dart:async';
+
+class StopWatchModel extends ChangeNotifier {
+  bool isStopPressed = true;
+  bool isResetPressed = true;
+  bool isStartPressed = true;
+
+  String stopWatchTimeDisplay = '00:00:00:00';
+  // Stopwatch型を指定
+  Stopwatch swatch = Stopwatch();
+  final dul = const Duration(milliseconds: 10);
+
+  void startTimer() {
+    Timer(dul, keepRunning);
+  }
+
+  void keepRunning() {
+    if (swatch.isRunning) {
+      startTimer();
+    }
+    int milliSeconds = ((swatch.elapsedMilliseconds / 10).floor() % 100);
+    this.stopWatchTimeDisplay =
+        (swatch.elapsed.inHours).toString().padLeft(2, '0') +
+            ':' +
+            (swatch.elapsed.inMinutes % 60).toString().padLeft(2, '0') +
+            ':' +
+            (swatch.elapsed.inSeconds % 60).toString().padLeft(2, '0') +
+            ':' +
+            (milliSeconds).toString().padLeft(2, '0');
+    notifyListeners();
+  }
+
+  startStopWatch() {
+    this.isStopPressed = false;
+    this.isStartPressed = false;
+    this.isResetPressed = false;
+
+    swatch.start();
+    startTimer();
+    notifyListeners();
+  }
+
+  stopStopWatch() {
+    this.isStopPressed = true;
+    this.isResetPressed = false;
+    this.isStartPressed = true;
+
+    swatch.stop();
+    notifyListeners();
+  }
+
+  resetStopWatch() {
+    this.isResetPressed = true;
+    this.isStartPressed = true;
+    this.isStopPressed = true;
+
+    swatch.stop();
+    swatch.reset();
+    stopWatchTimeDisplay = '00:00:00:00';
+    notifyListeners();
+  }
+}
