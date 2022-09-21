c190ce2c39cdba4ec04301143f3b309570bdcecc
ume_bar <kagari0915@gmail.com>
2022-09-21 09:54:50 +0900

    add envDart

---
+++ test_flutter/lib/env.dart
@@ -0,0 +1,6 @@
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+
+String? domain = dotenv.env['KINTONE_DOMAIN'];
+String? baseUrl = dotenv.env['KINTONE_BASE_URL'];
+String? apiToken = dotenv.env['KINTONE_APITOKEN'];
+String? id = dotenv.env['KINTONE_ID'];
