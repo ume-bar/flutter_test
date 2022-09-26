bf092bf3615f14fb05a04f532bc8abd02fa37ce9
ume_bar <kagari0915@gmail.com>
2022-09-26 14:36:51 +0900

    add json test

--- test_flutter/lib/main.dart
+++ test_flutter/lib/main.dart
@@ -5,6 +5,7 @@ import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:intl/intl.dart';
 import 'package:test_flutter/states/kintone_api.dart';
 import 'package:test_flutter/views/home.dart';
+import 'dart:convert' as convert;
 
 Future<void> main() async {
   // メッセージの翻訳、日本のロケールにする。
@@ -15,10 +16,54 @@ Future<void> main() async {
   SystemChrome.setEnabledSystemUIMode(SystemUiMode.immersive);
   //　起動時に読み込むenvファイルを指定
   await dotenv.load(fileName: ".env");
+
+  final map = {"firstName": "ume", "lastName": "tatsu", "age": 39};
+  // Map => json文字列
+  final json = convert.json.encode(map);
+  print("XXXXXXXXXXXXXXXXXXX");
+  print(json); // {"firstName": "ume", "lastName": "tatsu", "age": 39}
+
+  // json文字列 => Map
+  final mapNew = convert.json.decode(json) as Map<String, dynamic>;
+  print(mapNew); // {firstName: ume, lastName: tatsu, age: 39}
+
+  final Map<String, dynamic> encodableMap = {
+    "firstName": "ume",
+    "lastName": "sou",
+    "age": 5
+  };
+  final String encodedMap = convert.json.encode(encodableMap);
+  print(encodedMap);
+
+  Person p = new Person("ume", "haji", 0);
+  print("Person:${convert.json.encode(p)}");
+
+  // 変換不可能なオブジェクトが渡されたときの挙動を指定
+  Object unencodable = new Object();
+  try {
+    print(convert.json.encode(unencodable, toEncodable: (e) => e.toString()));
+  } on convert.JsonUnsupportedObjectError {
+    print("Error");
+  }
+
   // runAppの後にProviderScopeを置かないとriverpodのエラーが出る。
   runApp(const ProviderScope(
     child: MyApp(),
   ));
+}
+
+// カスタムクラスを変換したい場合は、クラスにtoJsonメソッドを実装する
+class Person {
+  String firstName;
+  String lastName;
+  int age;
+  Person(this.firstName, this.lastName, this.age);
+
+  // toJsonメソッドを実装しておくとencode時に呼ばれる
+  // JSON文字列へと変換可能なオブジェクトを返す
+  // ここではMap型を返している
+  dynamic toJson() =>
+      {"firstName": firstName, "lastName": lastName, "age": age};
 }
 
 class MyApp extends StatelessWidget {
