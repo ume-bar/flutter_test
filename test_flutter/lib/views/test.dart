3bc9d2272e5903bea1f7cf7cd9445f3b9bee905f
ume_bar <kagari0915@gmail.com>
2022-10-05 14:07:05 +0900

    add form

--- test_flutter/lib/views/test.dart
+++ test_flutter/lib/views/test.dart
@@ -35,46 +35,95 @@ class TestView extends HookConsumerWidget {
     final text = useState('');
 
     return Scaffold(
+      resizeToAvoidBottomInset: true,
       appBar: AppBar(title: Text(title)),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              '${state.count}',
-              style: Theme.of(context).textTheme.headline4,
+      body: Stack(children: [
+        SingleChildScrollView(
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Text(
+                  '${state.count}',
+                  style: Theme.of(context).textTheme.headline4,
+                ),
+                Container(
+                    padding: const EdgeInsets.all(50.0),
+                    child: Column(
+                      children: <Widget>[
+                        Text(
+                          '${text.value}',
+                          style: TextStyle(
+                              color: Colors.blueAccent,
+                              fontSize: 30.0,
+                              fontWeight: FontWeight.w500),
+                        ),
+                        new TextField(
+                            controller: _controller,
+                            enabled: true,
+                            maxLength: 10,
+                            maxLengthEnforcement: MaxLengthEnforcement.enforced,
+                            style: TextStyle(color: Colors.red),
+                            obscureText: false,
+                            maxLines: 1,
+                            onChanged: (String e) {
+                              text.value = e;
+                            }),
+                        new TextField(
+                            controller: _controller,
+                            enabled: true,
+                            maxLength: 10,
+                            maxLengthEnforcement: MaxLengthEnforcement.enforced,
+                            style: TextStyle(color: Colors.red),
+                            obscureText: false,
+                            maxLines: 1,
+                            onChanged: (String e) {
+                              text.value = e;
+                            }),
+                        new TextField(
+                            controller: _controller,
+                            enabled: true,
+                            maxLength: 10,
+                            maxLengthEnforcement: MaxLengthEnforcement.enforced,
+                            style: TextStyle(color: Colors.red),
+                            obscureText: false,
+                            maxLines: 1,
+                            onChanged: (String e) {
+                              text.value = e;
+                            }),
+                      ],
+                    )),
+              ],
             ),
-            Container(
-                padding: const EdgeInsets.all(50.0),
-                child: Column(
-                  children: <Widget>[
-                    Text(
-                      '${text.value}',
-                      style: TextStyle(
-                          color: Colors.blueAccent,
-                          fontSize: 30.0,
-                          fontWeight: FontWeight.w500),
-                    ),
-                    new TextField(
-                        controller: _controller,
-                        enabled: true,
-                        maxLength: 10,
-                        maxLengthEnforcement: MaxLengthEnforcement.enforced,
-                        style: TextStyle(color: Colors.red),
-                        obscureText: false,
-                        maxLines: 1,
-                        onChanged: (String e) {
-                          text.value = e;
-                        }),
-                  ],
-                )),
-          ],
+          ),
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: provider.countUp,
-        child: const Icon(Icons.add),
-      ),
+        Align(
+            alignment: Alignment.bottomCenter,
+            child: Container(
+                height: 47,
+                width: 297,
+                margin: EdgeInsets.only(bottom: 20),
+                decoration: BoxDecoration(
+                    color: Colors.white,
+                    border: Border.all(color: Colors.white),
+                    borderRadius: BorderRadius.circular(10),
+                    boxShadow: [
+                      BoxShadow(color: Color(0x19000000), blurRadius: 14)
+                    ]),
+                child:
+                    Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+                  GestureDetector(
+                      onTap: () {
+                        Navigator.pop(context);
+                      },
+                      child: Text('戻る',
+                          style: TextStyle(fontSize: 18, color: Colors.black))),
+                ])))
+      ]),
+      // floatingActionButton: FloatingActionButton(
+      //   onPressed: provider.countUp,
+      //   child: const Icon(Icons.add),
+      // ),
     );
   }
 }
