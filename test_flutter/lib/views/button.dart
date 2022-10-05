e573acd10c580094aa599cc97fe7f642a43a6359
ume_bar <kagari0915@gmail.com>
2022-10-05 16:53:06 +0900

    add 6-button

--- test_flutter/lib/views/button.dart
+++ test_flutter/lib/views/button.dart
@@ -10,7 +10,80 @@ class ButtonView extends HookConsumerWidget {
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[],
+          children: <Widget>[
+            ElevatedButton(
+              child: const Text('Button'),
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.blueAccent,
+                foregroundColor: Colors.white,
+              ),
+              onPressed: () {},
+            ),
+            SizedBox(
+              width: 5,
+            ),
+            ElevatedButton(
+              child: const Text('Button'),
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.blueAccent,
+                foregroundColor: Colors.white,
+                shape: const StadiumBorder(),
+              ),
+              onPressed: () {},
+            ),
+            SizedBox(
+              width: 5,
+            ),
+            OutlinedButton(
+              child: const Text('Button'),
+              style: OutlinedButton.styleFrom(
+                foregroundColor: Colors.black,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                side: const BorderSide(color: Colors.blueAccent),
+              ),
+              onPressed: () {},
+            ),
+            SizedBox(
+              width: 5,
+            ),
+            OutlinedButton(
+              child: const Text('Button'),
+              style: OutlinedButton.styleFrom(
+                foregroundColor: Colors.black,
+                shape: const StadiumBorder(),
+                side: const BorderSide(color: Colors.blueAccent),
+              ),
+              onPressed: () {},
+            ),
+            SizedBox(
+              width: 5,
+            ),
+            ElevatedButton(
+              child: const Text('Button'),
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.grey[200],
+                foregroundColor: Colors.blueAccent,
+              ),
+              onPressed: () {},
+            ),
+            SizedBox(
+              width: 5,
+            ),
+            ElevatedButton.icon(
+              icon: const Icon(
+                Icons.star_outline,
+                color: Colors.blueAccent,
+              ),
+              label: const Text('Button'),
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.grey[200],
+                foregroundColor: Colors.blueAccent,
+              ),
+              onPressed: () {},
+            ),
+          ],
         ),
       ),
       floatingActionButton: FloatingActionButton(
